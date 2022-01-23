1514dc417350ace5fdcde232c18a83c975f36011
PerryLewis02 <pl496@cam.ac.uk>
2022-01-23 17:14:00 +0000

    Complete Task 1B

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -5,5 +5,16 @@
 geographical data.
 
 """
-
+import numpy as np
 from .utils import sorted_by_key  # noqa
+
+from .station import MonitoringStation
+
+
+def stations_by_distance(stations, p):
+    locations = np.array([station.coord for station in stations])
+    # using haversine formula
+    distanceToP = 2*6371000*np.arcsin(np.sqrt(np.sin((p[0] - locations[:,0])/2)**2 + np.cos(p[0]) * np.cos(locations[:,0])*np.sin((p[1] - locations[:,1])/2)**2))
+    # using r as 6371km
+    return distanceToP
+    

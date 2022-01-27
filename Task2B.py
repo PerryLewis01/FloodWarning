280f3e4259f161fa4a5b555395d0afbea2c9da86
FelixDaines <fd387@cam.ac.uk>
2022-01-27 11:58:51 +0000

    Task completed

---
+++ Task2B.py
@@ -0,0 +1,20 @@
+from floodsystem.flood import stations_level_over_threshold
+from floodsystem.stationdata import build_station_list, update_water_levels
+
+
+def run():
+    # Build list of stations
+    stations = build_station_list()
+
+    # Update latest level data for all stations
+    update_water_levels(stations)
+
+    for i in stations_level_over_threshold(stations, 0.8):
+        print("{}  {}".format(i[0], i[1]))
+
+    if len(stations_level_over_threshold(stations, 0.8)) == 0 :
+        print("No stations over tolerance")
+
+if __name__ == "__main__":
+    print("*** Task 2B: CUED Part IA Flood Warning System ***")
+    run()

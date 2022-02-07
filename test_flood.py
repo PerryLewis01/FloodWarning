1caaa6044e47d5c98a8426eecba09a1ac6e0cc2f
FelixDaines <fd387@cam.ac.uk>
2022-02-07 12:19:33 +0000

    working on 2B tests

--- test_flood.py
+++ test_flood.py
@@ -20,6 +20,14 @@ Test_station = [MonitoringStation(
         coord=(float(50), float(0)),
         typical_range=(15, 2),
         river='River_test',
+        town='test_town'),
+        MonitoringStation(
+        station_id=3,
+        measure_id=30,
+        label='test_station_3',
+        coord=(float(56), float(0)),
+        typical_range=(1, 19),
+        river='River_test',
         town='test_town')]
 
 def test_stations_highest_rel_level():
@@ -28,3 +36,12 @@ def test_stations_highest_rel_level():
     Test_station[1].latest_level = 255
     assert flood.stations_highest_rel_level(Test_station, 1) == [('test_station', -0.10000000000000009)]
 
+
+def test_stations_level_over_threshold():
+    Test_station[0].latest_level = 10
+    Test_station[2].latest_level = 255
+
+    print(flood.stations_level_over_threshold(Test_station, 0.1))
+    print(flood.stations_level_over_threshold(Test_station, 0.1))
+
+    assert flood.stations_level_over_threshold(Test_station, 1.1)[0][1] > flood.stations_level_over_threshold(Test_station, 1.1)[1][1]

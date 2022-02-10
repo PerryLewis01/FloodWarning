3d9e021ea71067a5ff206415eabf55aea359b0cc
PerryLewis02 <pl496@cam.ac.uk>
2022-02-10 15:52:24 +0000

    fix task output

--- Task1E.py
+++ Task1E.py
@@ -7,7 +7,7 @@ def run():
     # Build list of stations
     stations = build_station_list()
 
-    print(geo.rivers_by_station_number(stations, 9))
+    print(geo.rivers_by_station_number(stations, 10))
 
 
 if __name__ == "__main__":
